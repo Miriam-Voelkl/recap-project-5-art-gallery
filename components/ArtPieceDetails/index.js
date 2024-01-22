fac57a3f7e602b210d38b9781ead802d35f5c99d
Eileen Ilsemann <eileenilsemanni@gmail.com>
2024-01-22 15:11:03 +0100

    styling details page

--- components/ArtPieceDetails/index.js
+++ components/ArtPieceDetails/index.js
@@ -1,5 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  border: 1px solid black;
+  padding: 2px;
+  border-radius: 5px;
+  margin: 5px;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const StyledList = styled.ul`
+  margin-bottom: 5px;
+`;
 
 export default function ArtPieceDetails({
   image,
@@ -11,19 +27,20 @@ export default function ArtPieceDetails({
 }) {
   return (
     <>
+      <h1>Details</h1>
       <Image
         src={image}
-        height={dimensions.height / 10}
-        width={dimensions.width / 10}
+        height={dimensions.height / 5}
+        width={dimensions.width / 5}
         alt={title}
       ></Image>
-      <ul>
+      <StyledList>
         <li> {title}</li>
         <li> {artist}</li>
         <li> {year}</li>
         <li> {genre}</li>
-      </ul>
-      <Link href="/art-pieces">Back to gallery</Link>
+      </StyledList>
+      <StyledLink href="/art-pieces">Back to gallery</StyledLink>
     </>
   );
 }
